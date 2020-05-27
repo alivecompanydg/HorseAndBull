cd546f697fd292da8983a63b6e337f1df3ec396b
Deive <alivecompanydg@gmail.com>
2020-05-27 16:50:57 -0300

    criação do objeto 'game'

--- frontend/src/scripts/vqjdState.js
+++ frontend/src/scripts/vqjdState.js
@@ -6,6 +6,57 @@ let horse = ""
 let horseVelocity = 0
 let selectedMode = ""
 
+let roomNumber = 0
+
 const state = {
     rooms:[]
 }
+
+function createGame() {
+
+    let number = 0
+    
+    function addPlayer(playerData) {
+
+        const playerName = playerData.name
+
+        const player = {
+            playerId: playerData.name,
+            horse:horse,
+            horseVelocity,
+            selectedMode
+        }
+
+        if(state.rooms[number].players.length === 2){
+            addRoom(roomNumber)
+            state.rooms[roomNumber-1].players.push(player)
+        }else{
+            state.rooms[number].players.push(player)
+        }
+    }
+
+    function addRoom(roomIndex) {
+
+        number = roomIndex
+
+        state.rooms[roomIndex] = {
+            players:[]
+        }
+
+        roomNumber++
+    }
+
+    if(!state.rooms.length){
+        addRoom(roomNumber)
+        addPlayer(playerData)
+    }
+
+    return {
+        addPlayer,
+        number,
+        addRoom
+    }
+
+}
+
+let game = ""
